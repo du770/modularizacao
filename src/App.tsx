5481ce732c810982b1dd462a033f31d03dbcdd9c
du770 <eduarda.s.lopess@hotmail.com>
2025-07-23 11:37:41 -0300

    3

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-
+import { dividir, somar } from './utils/math'
 import './App.css'
 
 function App() {
 
   return (
     <>
-    </>
+      <div>
+      <h1>Soma: {somar(4, 2)}</h1>
+      <h1>Divisão: {dividir(10, 2)}</h1>
+     ^ 
+    </div>
+    </> 
   )
 }
 
 export default App
+
+  
